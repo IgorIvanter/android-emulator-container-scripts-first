4226cbcec327fbafef31c6dae5c316b70e21aa3b
Igor Ivanter <igor.ivanter@gmail.com>
2023-06-17 18:44:29 +0000

    - Deleted junk file pbcopy
    - Remade the js/src/components/login_firebase.js component, switched from Google sign-in with pop-up to email + password login.
    
    - Temporarily created a new js/src/config_new.js that js/src/components/login_firebase.js now uses to read the firebase config object (previously from js/src/config.js)

--- js/src/components/login_firebase.js
+++ js/src/components/login_firebase.js
@@ -1,4 +1,4 @@
-import withStyles from '@mui/styles/withStyles';
+import withStyles from "@mui/styles/withStyles";
 
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
@@ -10,90 +10,150 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import PropTypes from "prop-types";
 import React from "react";
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import {
-    FirebaseAuthProvider,
-    FirebaseAuthConsumer
+	FirebaseAuthProvider,
+	FirebaseAuthConsumer,
 } from "@react-firebase/auth";
-import { config } from "../config";
+import { config } from "../config_new";
 
-const useStyles = theme => ({
-    "@global": {
-        body: {
-            backgroundColor: theme.palette.common.white
-        }
-    },
-    paper: {
-        marginTop: theme.spacing(8),
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center"
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main
-    },
-    form: {
-        width: "100%", // Fix IE 11 issue.
-        marginTop: theme.spacing(1)
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2)
-    }
+const useStyles = (theme) => ({
+	"@global": {
+		body: {
+			backgroundColor: theme.palette.common.white,
+		},
+	},
+	paper: {
+		marginTop: theme.spacing(8),
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+		width: "100%", // Fix IE 11 issue.
+		marginTop: theme.spacing(1),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
 });
 
 // A basic login page using firebase.
 class SignIn extends React.Component {
-    state = {
-        email: "",
-        password: "",
-    };
+	state = {
+		email: "",
+		password: "",
+	};
 
-    static propTypes = {
-        auth: PropTypes.object.isRequired // Auth service
-    };
+	static propTypes = {
+		auth: PropTypes.object.isRequired, // Auth service
+	};
 
-    loginGoogle = () => {
-        const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithPopup(googleAuthProvider);
-    }
+	render() {
+		const { classes, auth } = this.props;
+		const { email, password } = this.state;
 
-    render() {
-        const { classes, auth } = this.props;
+		const handleFormSubmit = (event) => {
+			event.preventDefault();
+			firebase
+				.auth()
+				.signInWithEmailAndPassword(email, password)
+				.then((userCredential) => {
+					// User signed in
+					const user = userCredential.user;
+					firebase
+						.auth()
+						.currentUser.getIdToken()
+						.then((token) => {
+							auth.setToken(`Bearer ${token}`);
+						});
+				})
+				.catch((error) => {
+					// Handle login errors
+					console.error(error);
+				});
+		};
 
-        return (
-            <FirebaseAuthProvider {...config} firebase={firebase}>
-                <Container component="main" maxWidth="xs">
-                    <CssBaseline />
-                    <div className={classes.paper}>
-                        <Avatar className={classes.avatar}>
-                            <LockOutlinedIcon />
-                        </Avatar>
-                        <Typography component="h1" variant="h5">Sign in</Typography>
-                        <Box mt={8}>
-                            <Button variant="contained" color="primary" onClick={this.loginGoogle} >Sign in with Google</Button>
-                        </Box>
-                    </div>
-                    <Box mt={8}>
-                        <Copyright />
-                    </Box>
-                </Container>
-                <FirebaseAuthConsumer>
-                    {({ isSignedIn, user, providerId }) => {
-                        console.log(`Firebase login: isSignedId: ${isSignedIn}`)
-                        if (isSignedIn) {
-                            firebase.auth().currentUser.getIdToken().then(function (token) {
-                                auth.setToken(`Bearer ${token}`);
-                            });
+		const handleInputChange = (event) => {
+			const { name, value } = event.target;
+			this.setState({ [name]: value });
+		};
 
-                        }
-                    }}
-                </FirebaseAuthConsumer>
-            </FirebaseAuthProvider>
-
-        );
-    }
+		return (
+			<FirebaseAuthProvider {...config} firebase={firebase}>
+				<Container component="main" maxWidth="xs">
+					<CssBaseline />
+					<div className={classes.paper}>
+						<Avatar className={classes.avatar}>
+							<LockOutlinedIcon />
+						</Avatar>
+						<Typography component="h1" variant="h5">
+							Sign in
+						</Typography>
+						<form className={classes.form} onSubmit={handleFormSubmit}>
+							<TextField
+								variant="outlined"
+								margin="normal"
+								required
+								fullWidth
+								id="email"
+								label="Email Address"
+								name="email"
+								autoComplete="email"
+								autoFocus
+								value={email}
+								onChange={handleInputChange}
+							/>
+							<TextField
+								variant="outlined"
+								margin="normal"
+								required
+								fullWidth
+								name="password"
+								label="Password"
+								type="password"
+								id="password"
+								autoComplete="current-password"
+								value={password}
+								onChange={handleInputChange}
+							/>
+							<Button
+								type="submit"
+								fullWidth
+								variant="contained"
+								color="primary"
+								className={classes.submit}
+							>
+								Sign In
+							</Button>
+						</form>
+					</div>
+					<Box mt={8}>
+						<Copyright />
+					</Box>
+				</Container>
+				<FirebaseAuthConsumer>
+					{({ isSignedIn, user, providerId }) => {
+						console.log(`Firebase login: isSignedId: ${isSignedIn}`);
+						if (isSignedIn) {
+							firebase
+								.auth()
+								.currentUser.getIdToken()
+								.then((token) => {
+									auth.setToken(`Bearer ${token}`);
+								});
+						}
+					}}
+				</FirebaseAuthConsumer>
+			</FirebaseAuthProvider>
+		);
+	}
 }
 
 export default withStyles(useStyles)(SignIn);
---
+++ js/src/config_new.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyD_13NL9MtgsVyx56jUDxuKJq3cl7KVgxw",
+  authDomain: "big-dick-emulator.firebaseapp.com",
+  projectId: "big-dick-emulator",
+  storageBucket: "big-dick-emulator.appspot.com",
+  messagingSenderId: "40922260601",
+  appId: "1:40922260601:web:f7fcd33021c096cc2620a8"
+};
